e402cdd6f2d3cd5c786332acfdc798466cbd9868
adam chnina <adam@chnina.com>
2026-05-25 15:45:13 +0100

    fix port

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,10 @@ const cors = require("cors");
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server running on port ${PORT}`);
+});
+
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
